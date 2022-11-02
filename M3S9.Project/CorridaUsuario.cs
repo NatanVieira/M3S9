4b13b2964ee125491742d7b70fdd19d88cf77b57
natanael Vieira <natanaelvieira130@gmail.com>
2022-11-02 15:04:59 -0300

    M3S9 - Exercicio 05

---
+++ M3S9.Project/CorridaUsuario.cs
@@ -0,0 +1,10 @@
+namespace  M3S9.Project
+{
+    public class CorridaUsuario{
+        public bool CorridaEmAndamento { get; set; }
+
+        public bool VerificaCorridaEmAndamento() {
+            return CorridaEmAndamento;
+        }
+    }
+}
